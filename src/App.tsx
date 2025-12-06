fbef483638be10d4fd054f7c3a4fa020a63ee45f
Ethan Flow <eflow1280@gmail.com>
2025-12-06 08:53:46 -0600

    Initialized Daisy UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <div>
         <h1 className="text-2xl font-bold underline">Vite + React</h1>
+        <button className="btn-ghost">Press Me</button>
       </div>
     </>
   );
